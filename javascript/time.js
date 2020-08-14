1a4b21e874fa4aa72781f4709081b9c66537ff44
user <laanov.art@yandex.ru>
2020-08-14 17:53:46 +0300

    new page web clock

---
+++ javascript/time.js
@@ -0,0 +1,15 @@
+const time = document.getElementById('time')
+
+// Function show time
+function showTime() {
+    let today = new Date(),
+        hour = today.getHours(),
+        min = today.getMinutes(),
+        sec = today.getSeconds()
+
+
+    time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec}`
+
+    setTimeout(showTime, 1000)
+}
+showTime()
