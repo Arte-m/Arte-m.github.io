8004aa188b4121bfa0f0b269beebadcc3613d525
Artem <65282091+Arte-m@users.noreply.github.com>
2020-10-07 11:37:29 +0300

    Delete config.js

--- .vuepress/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  base: "/github-pages-vuepress/",
-  title: "VuePress in Github Pages",
-  description:
-    "Static website built with VuePress and deployed on Github Pages",
-  dest: "./docs",
-};
