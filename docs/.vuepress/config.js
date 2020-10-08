dbe35bfdeb853b7b80302cc87f94461524ecb32f
Artem <65282091+Arte-m@users.noreply.github.com>
2020-10-08 12:07:41 +0300

    1

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "Arte-m Docs",
+  destination: "Just playing",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide" },
+      { text: "About me", link: "https://www.ya.ru" },
+    ],
+  },
+};
